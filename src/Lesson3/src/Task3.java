2b72647368f386f7edd9fadf48ccef47a2496fc8
Elvis Usmonov <666usmonof666@gmail.com>
2023-10-12 23:20:53 +0300

    mine commit

---
+++ src/Lesson3/src/Task3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sss = new Scanner(System.in);
+        String sentence;
+
+        System.out.print("Напишите любое предложение: ");
+        sentence = sss.nextLine();
+
+        System.out.println(length(sentence));
+    }
+    public static String[] splitt(String sentence){
+        String[]  words = sentence.split(" ");
+        return words;
+    }
+    public static int length(String sentence) {
+        String[] spaceLessText = splitt(sentence);
+        int length = spaceLessText.length;
+        return spaceLessText.length;
+    }
+}
