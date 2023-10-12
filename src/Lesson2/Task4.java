0f8546a9a801a88373637bee2d217e81ba236a0a
Elvis Usmonov <666usmonof666@gmail.com>
2023-10-12 23:16:13 +0300

    mine commit

---
+++ src/Lesson2/Task4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        theSmallest();
+    }
+
+    public static void theSmallest() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double a = input.nextFloat();
+
+        System.out.print("Введите второе число: ");
+        double b = input.nextFloat();
+
+        System.out.print("Введите третье число: ");
+        double c = input.nextFloat();
+
+        String theSmallest = Math.abs(a) < Math.abs(b) ?
+                Math.abs(a) < Math.abs(c) ? "Число " + a + " меньшее":
+                        "Число " + c + " меньшее" : Math.abs(b) < Math.abs(c) ?
+                "Число " + b + " меньшее" : "Число " + c + " меньшее";
+        System.out.println(theSmallest);
+    }
+}
